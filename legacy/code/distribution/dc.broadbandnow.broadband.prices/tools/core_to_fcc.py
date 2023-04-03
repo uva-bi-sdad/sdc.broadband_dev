80e8daa9168c9696d437087ac8b77c95197e6b2e
Alan <ahw9f@virginia.edu>
2023-04-03 11:52:47 -0400

    developing core_to_fcc pipeline

--- legacy/code/distribution/dc.broadbandnow.broadband.prices/tools/core_to_fcc.py
+++ legacy/code/distribution/dc.broadbandnow.broadband.prices/tools/core_to_fcc.py
@@ -12,12 +12,13 @@ import warnings
 import logging
 import pathlib
 import psycopg2
+import shutil
 from decouple import config
 
 # import traceback
 
 
-def download_data(county_fips, output_file):
+def download_data(county_fips, temp_dir):
     # decouple so that passwords are not stored
     conn = psycopg2.connect(
         pg_connection_dict={
@@ -29,18 +30,25 @@ def download_data(county_fips, output_file):
         }
     )
     cur = conn.cursor()
+
+    temp_path = os.path.join(temp_dir, "%s.csv" % county_fips)
+    # save csv to file given county_fips code
     cur.execute(
-        "\copy (SELECT situs_address,geoid_cnty FROM %s WHERE geoid_cnty = '%s') TO '~/%s.csv' CSV header;"
-        % (config("dbname"), county_fips, county_fips)
+        "\copy (SELECT situs_address,geoid_cnty FROM %s WHERE geoid_cnty = '%s') TO '%s' CSV header;"
+        % (config("dbname"), county_fips, temp_path)
     )
-    sql = "COPY (SELECT * FROM a_table WHERE month=6) TO STDOUT WITH CSV DELIMITER ';'"
-    with open("/mnt/results/month/table.csv", "w") as file:
-        cur.copy_expert(sql, file)
+    # sql = "COPY (SELECT * FROM a_table WHERE month=6) TO STDOUT WITH CSV DELIMITER ';'"
+    # with open("/mnt/results/month/table.csv", "w") as file:
+    #     cur.copy_expert(sql, file)
     cur.close()
     conn.close()
 
+    return pd.read_csv(
+        temp_path,
+    )
 
-def main(county_fip, output_file, force):
+
+def main(county_fip, output_file, temp_dir, force):
     warnings.filterwarnings("ignore")
     state_fips = pd.read_csv(
         "https://raw.githubusercontent.com/uva-bi-sdad/national_address_database/main/data/fips_state.csv",
@@ -51,15 +59,16 @@ def main(county_fip, output_file, force):
         state_fips[state_fips["fips"] == county_fip[:2]]["abbr"].values[0].upper()
     )
     county_fips = pd.read_csv(
-        "https://github.com/uva-bi-sdad/national_address_database/raw/main/data/fips_county.csv.xz",
+        "https://github.com/uva-bi-sdad/national_address_database/raw/main/data/fips_county.csv",
         dtype={"fips": object},
     )
     county = county_fips[county_fips["fips"] == county_fip].values[0]
 
-    df = download_data(county_fip)
+    df = download_data(county_fip, temp_dir)
     df = df.dropna()
     bdf = pd.DataFrame()
-    # # load csv's where the fips is a string
+
+    # Clean up the csv and export the results
 
     bdf["street"] = df["situs_address"].apply(lambda x: x.split(" %s" % state_abbr)[0])
     bdf["county"] = county
@@ -72,13 +81,13 @@ def main(county_fip, output_file, force):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Given a corelogic template, convert to a csv that can be queried to the fcc"
+        description="Given a corelogic template, convert to a csv that can be queried to the fcc. First downloads raw data into the temporary directory, then transforms it to a cleaned format in the output filepath"
     )
     parser.add_argument(
         "-i",
-        "--input_file",
+        "--input_county_fips",
         type=str,
-        help="The input csv",
+        help="The county fips to filter from the database",
         required=True,
     )
     parser.add_argument(
@@ -87,6 +96,14 @@ if __name__ == "__main__":
         type=str,
         help="The output csv",
         required=True,
+    )
+    parser.add_argument(
+        "-t",
+        "--temp_dir",
+        type=str,
+        help="The temporary directory to store raw downloaded files",
+        default=".temp",
+        required=False,
     )
     parser.add_argument(
         "-v",
@@ -102,6 +119,14 @@ if __name__ == "__main__":
         required=False,
         default=False,
     )
+    parser.add_argument(
+        "-ft",
+        "--force_delete_temp",
+        action=argparse.BooleanOptionalAction,
+        help="Force delete the temporary directory",
+        required=False,
+        default=False,
+    )
 
     args = parser.parse_args()
     log_level = logging.INFO
@@ -110,9 +135,19 @@ if __name__ == "__main__":
 
     logging.basicConfig(format="%(levelname)s: %(message)s", level=log_level)
 
-    assert os.path.isfile(args.input_file)
     if not args.force:
         assert not os.path.isfile(args.output_file)
 
-    success = main(args.input_file, args.output_file, args.force)
+    if not args.force_delete_temp:
+        assert not os.path.isdir(args.temp_dir), (
+            "temporary directory %s already exists" % args.temp_dir
+        )
+    else:  # Force delete the temporary files
+        shutil.rmtree(args.temp_dir)
+    os.mkdir(args.temp_dir)
+
+    success = main(args.input_county_fips, args.output_file, args.temp_dir, args.force)
     print("[%s] Output to %s successful" % (success, args.output_file))
+
+    # Cleaning up temporary directory and its contents
+    shutil.rmtree(args.temp_dir)
