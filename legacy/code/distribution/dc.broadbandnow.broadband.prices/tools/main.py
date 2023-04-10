628fd41b53655242db8870bfd269f013cb3de903
Alan <ahw9f@virginia.edu>
2023-04-10 17:10:07 -0400

    saving to create combination script

---
+++ legacy/code/distribution/dc.broadbandnow.broadband.prices/tools/main.py
@@ -0,0 +1,21 @@
+from core_to_fcc import main as c2f
+
+
+"""
+Writing a wrapper that combines everything so that:
+
+Input:
+    1) The 5 digit fips code of the county in question, and 
+    2) The name of the county
+
+Output:
+    1) csv.xz of the broadbandnow results of the county
+    2) csv.xz of the spatial joined results of the broadbandnow-available data (for plotting)
+    
+
+# mkdir -p temp
+# mkdir -p temp_bbn
+
+"""
+
+# c2f(["-f", "/etc/hosts", "-t", "json"])
